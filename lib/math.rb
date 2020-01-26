92b895fd2c4c41eb59959891ba4cfee4c2d6470c
isabel k. lee <hello@isabelklee.co>
2020-01-26 04:47:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,12 +4,12 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  subtr = num2 - num1
+  subtr = num1 - num2
   return subtr
 end
 
 def division(num1, num2)
-  divi = num2 / num1
+  divi = num1 / num2
   return divi
 end
 
@@ -19,7 +19,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  mod = num2 % num1
+  mod = num1 % num2
   return mod
 end
 
